4b382851954c747aeb8babbe640d7b8737d211db
Microstar <xoybek7@gmail.com>
2022-04-20 20:58:03 +0500

    lesson_24

---
+++ lesson_24/app/src/main/java/uz/micro/star/lesson_24/MainActivity.kt
@@ -0,0 +1,32 @@
+package uz.micro.star.lesson_24
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.widget.Toast
+import uz.micro.star.mixlib.MySwitchButton
+import uz.micro.star.mixlib.NextBtnView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<uz.micro.star.mixlib.MySwitchButton>(R.id.mySwitch).setOnSwitchListener {
+            Log.d("DDDD", "onCreate: ${it}")
+        }
+                val btn=findViewById<uz.micro.star.mixlib.NextBtnView>(R.id.nextBtn)
+        btn.setButtonClickListener {
+            Toast.makeText(this, "Yesss", Toast.LENGTH_SHORT).show()
+            object : CountDownTimer(1000, 1000) {
+                override fun onFinish() {
+                    btn.dismissLoader()
+                }
+
+                override fun onTick(millisUntilFinished: Long) {
+
+                }
+            }.start()
+        }
+    }
+}
