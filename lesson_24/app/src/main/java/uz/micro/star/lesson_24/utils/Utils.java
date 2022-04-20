914aa67f0b417a65e47105f080f499fe8118d53d
Microstar <xoybek7@gmail.com>
2022-04-20 20:57:48 +0500

    lesson_24

---
+++ lesson_24/app/src/main/java/uz/micro/star/lesson_24/utils/Utils.java
@@ -0,0 +1,14 @@
+package uz.micro.star.lesson_24.utils;
+
+import android.content.res.Resources;
+
+
+public final class Utils {
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    public static int pxToDp(int px) {
+        return (int) (px / Resources.getSystem().getDisplayMetrics().density);
+    }
+}
