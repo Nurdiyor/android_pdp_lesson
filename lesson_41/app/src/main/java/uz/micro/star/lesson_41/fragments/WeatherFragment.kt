3945972c4a80bb8a1a830c635df9c156d011ab4d
Microstar <xoybek7@gmail.com>
2022-06-13 20:43:56 +0500

    lesson_42

--- lesson_41/app/src/main/java/uz/micro/star/lesson_41/fragments/WeatherFragment.kt
+++ lesson_41/app/src/main/java/uz/micro/star/lesson_41/fragments/WeatherFragment.kt
@@ -2,6 +2,10 @@ package uz.micro.star.lesson_41.fragments
 
 import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenCreated
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import uz.micro.star.lesson_41.databinding.FragmentWeatherBinding
 
 class WeatherFragment : BaseFragment<FragmentWeatherBinding>(
@@ -9,13 +13,23 @@ class WeatherFragment : BaseFragment<FragmentWeatherBinding>(
 ) {
     override fun onViewCreate() {
 
-        viewModel.errorMessageLiveData.observe(viewLifecycleOwner){
+        viewModel.errorMessageLiveData.observe(viewLifecycleOwner) {
             Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
         }
-        viewModel.trainersListLiveData.observe(viewLifecycleOwner){
-            //
-            it.forEach {
-                Log.d("HHH","--------------${it.trainerName}----------------")
+//        viewModel.trainersListLiveData.observe(viewLifecycleOwner){
+//            //
+//            it.forEach {
+//                Log.d("HHH","--------------${it.trainerName}----------------")
+//            }
+//        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.whenCreated {
+                viewModel.trainersListChannel.collect {
+                    it.forEach {
+                        Log.d("HHH", "--------------${it.trainerName}----------------")
+                    }
+                }
             }
         }
         viewModel.getAllTrainers()
--- lesson_41/app/src/main/java/uz/micro/star/lesson_41/viewmodels/ActivityViewModel.kt
+++ lesson_41/app/src/main/java/uz/micro/star/lesson_41/viewmodels/ActivityViewModel.kt
@@ -1,11 +1,18 @@
 package uz.micro.star.lesson_41.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import uz.micro.star.lesson_41.retrofit.models.BaseNetworkResult
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import uz.micro.star.lesson_34.retrofit.models.response.TrainerResponse
 import uz.micro.star.lesson_41.repository.ActivityRepository
+import uz.micro.star.lesson_41.retrofit.models.BaseNetworkResult
 import uz.micro.star.lesson_41.utils.SharedPref
 import javax.inject.Inject
 
@@ -13,25 +20,30 @@ class ActivityViewModel @Inject constructor(
     private val activityRepository: ActivityRepository,
     private var sharedPreferencesHelper: SharedPref
 ) : ViewModel() {
-    private val trainersList = MutableLiveData<List<TrainerResponse>>()
-    val trainersListLiveData: LiveData<List<TrainerResponse>> get() = trainersList
-
+    //    private val trainersList = MutableLiveData<List<TrainerResponse>>()
+//    val trainersListLiveData: LiveData<List<TrainerResponse>> get() = trainersList
+    private val trainersList = Channel<List<TrainerResponse>>()
+    val trainersListChannel = trainersList.receiveAsFlow()
     private val errorMessage = MutableLiveData<String>()
     val errorMessageLiveData: LiveData<String> get() = errorMessage
 
     fun getAllTrainers() {
-        activityRepository.getAllTrainers().observeForever {
-            when (it) {
-                is BaseNetworkResult.Error -> {
-                    it.message?.let { error ->
-                        errorMessage.value = error
+        viewModelScope.launch {
+            activityRepository.getAllTrainers().catch {
+                Log.d("EXC", "getAllTrainers: exception")
+            }.collect {
+                when (it) {
+                    is BaseNetworkResult.Error -> {
+                        it.message?.let { error ->
+                            errorMessage.value = error
+                        }
                     }
-                }
-                is BaseNetworkResult.Success -> {
-                    it.data?.let { list ->
-                        trainersList.value = list
-                    }
+                    is BaseNetworkResult.Success -> {
+                        it.data?.let { list ->
+                            trainersList.send(list)
+                        }
 
+                    }
                 }
             }
         }
